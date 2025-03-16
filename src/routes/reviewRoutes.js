42f6c44c9407b00cc92baf21423ab681b00900bb
= <=>
2025-03-16 13:48:58 +0530

    changes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { createReview, deleteReview, getAllReviews, getReview, setTourAndUser, updateReview } from "../controllers/reviewController.js";
 import { protect, restrictTo } from "../controllers/authController.js";
+import { UserRole } from "../constants.js";
 
 const router = Router({mergeParams: true});
 router.use(protect);
 
 router.route('/')
     .get(getAllReviews)
-    .post(protect,restrictTo('user'),setTourAndUser,createReview);
+    .post(protect,restrictTo([UserRole.USER]),setTourAndUser,createReview);
 
 router.route('/:id')
-    .get(getReview).delete( restrictTo('user', 'admin'), deleteReview)
-    .patch( restrictTo('user', 'admin'), updateReview);
+    .get(getReview).delete( restrictTo([UserRole.USER,UserRole.ADMIN]), deleteReview)
+    .patch( restrictTo([UserRole.USER,UserRole.ADMIN]), updateReview);
 export default router;
 
